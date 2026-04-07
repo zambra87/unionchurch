035206c874738265c86e0a1a8623b338c68b4cd0
Daniel Zambra <daniel.zambra@me.com>
2026-04-07 17:33:58 -0400

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,9 @@ const { withSentryConfig } = require('@sentry/nextjs');
 
 const moduleExports = {
   reactStrictMode: true,
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
 
   images: {
     remotePatterns: [
